a68217c234a9c2c715b805782ca719f51de42270
Evgeniy PC <79169380904@mail.ru>
2025-02-13 23:24:16 +0300

    pr1: doing: HomeFragment

--- app/src/main/java/ru/trukhmanov/mirea/HomeActivity.kt
+++ app/src/main/java/ru/trukhmanov/mirea/HomeActivity.kt
@@ -3,18 +3,27 @@ package ru.trukhmanov.mirea
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    // Handle the home action
+                    true
+                }
+
+                R.id.cart -> {
+                    // Handle the dashboard action
+                    true
+                }
+                else -> false
+            }
         }
     }
 }

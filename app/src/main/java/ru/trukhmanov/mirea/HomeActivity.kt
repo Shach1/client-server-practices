fa754032e5cd55983f146bd2965f46a01bc2db3a
Evgeniy PC <79169380904@mail.ru>
2025-02-13 17:53:42 +0300

    pr1: complete: MainActivity

--- app/src/main/java/ru/trukhmanov/mirea/HomeActivity.kt
+++ app/src/main/java/ru/trukhmanov/mirea/HomeActivity.kt
@@ -1,31 +1,20 @@
 package ru.trukhmanov.mirea
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-    }
-
-    override fun onStart() {
-        super.onStart()
-        val btnGoHome = findViewById<Button>(R.id.btnGoHome)
-        btnGoHome.setOnClickListener {
-            startActivity(Intent(this, HomeActivity::class.java))
         }
     }
 }
